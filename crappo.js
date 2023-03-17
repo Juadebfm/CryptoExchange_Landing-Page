657dc514024337fff01af941de34778503789d40
Julius Adebowale <juadebgabriel@gmail.com>
2023-03-17 16:49:42 +0100

    sorting out scroll behavior

--- crappo.js
+++ crappo.js
@@ -1,37 +1,9 @@
-const navBtn = document.querySelector(".btn-icon");
-const closeBtn = document.querySelector(".close-btn");
-const sideBar = document.querySelector(".side-bar");
-const singleCard = document.querySelectorAll(".single-card");
-const cryptoBtn = document.querySelectorAll(".crypto-btn");
-const cryptoIcon = document.querySelectorAll(".crypto-icon");
+let navSec = document.getElementById("navSec");
 
-navBtn.addEventListener("click", function () {
-  ShowSideBar = sideBar.classList.contains("show-sidebar");
-  if (!ShowSideBar) {
-    sideBar.classList.add("show-sidebar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navSec.classList.add("navSecBg");
+  } else {
+    navSec.classList.remove("navSecBg");
   }
-});
-
-closeBtn.addEventListener("click", function () {
-  ShowSideBar = sideBar.classList.contains("show-sidebar");
-  if (ShowSideBar) {
-    sideBar.classList.remove("show-sidebar");
-  }
-  console.log(closeBtn);
-});
-
-// remove active class from each card
-function removeActive() {
-  singleCard.forEach(function (card) {
-    card.classList.remove("active");
-  });
-}
-
-// card event listener
-singleCard.forEach(function (card) {
-  card.addEventListener("click", function (e) {
-    removeActive();
-    // console.log(e.currentTarget);
-    const active = e.currentTarget.classList.add("active");
-  });
 });
