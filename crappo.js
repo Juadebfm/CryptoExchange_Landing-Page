1cdfc8331008fd8108ef432687b6872361c22d9a
Julius Adebowale <juadebgabriel@gmail.com>
2023-03-18 21:15:23 +0100

    adding the scoll effect

--- crappo.js
+++ crappo.js
@@ -7,3 +7,17 @@ window.addEventListener("scroll", () => {
     navSec.classList.remove("navSecBg");
   }
 });
+
+const logo = document.getElementById("logo");
+
+logo.addEventListener("click", (event) => {
+  event.preventDefault();
+  const scrollToTop = window.setInterval(() => {
+    const pos = window.pageYOffset;
+    if (pos > 0) {
+      window.scrollTo(0, pos - 50);
+    } else {
+      window.clearInterval(scrollToTop);
+    }
+  }, 16);
+});
