1ba09fa235534f701d0684a4dc5a2c9289fa9d98
Julius Adebowale <juadebgabriel@gmail.com>
2023-03-21 11:51:03 +0100

    fixed the toggle functionality for menu bars

--- crappo.js
+++ crappo.js
@@ -50,3 +50,21 @@ form.addEventListener("submit", (event) => {
     }, 2000);
   }
 });
+
+// Menu toggle button
+
+const toggleBtn = document.getElementsByClassName("bx-menu")[0];
+const closeBtn = document.getElementsByClassName("bx-x")[0];
+const navList = document.getElementsByClassName("navList")[0];
+
+toggleBtn.addEventListener("click", () => {
+  navList.classList.toggle("active");
+  toggleBtn.style.display = "none";
+  closeBtn.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  navList.classList.remove("active");
+  toggleBtn.style.display = "flex";
+  closeBtn.style.display = "none";
+});
